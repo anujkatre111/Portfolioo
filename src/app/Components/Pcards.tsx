98be8fa972b31ad5f57ba7b07b164bc7040c3549
Anuj Katre <anujkatre72@gmail.com>
2025-06-21 19:13:20 +0530

    imagealt

--- src/app/Components/Pcards.tsx
+++ src/app/Components/Pcards.tsx
@@ -4,7 +4,7 @@ const Pcards = () => {
   return (
     <>
     <div className='Project-Card flex flex-col border border-black p-[8px] w-[300px] rounded'>
-            <div className='flex justify-center items-center w-full h-[180px] bg-red-600'><img src=""/></div>
+            <div className='flex justify-center items-center w-full h-[180px] bg-red-600'><img src="" alt='nothing'/></div>
             <h1 className='text-[24px] font-medium tracking-tighter'>AI for Finance</h1>
             <h1 className='text-[16px] text-gray-600 tracking-tighter'>This is an AI assistant for finance</h1>
      </div>
